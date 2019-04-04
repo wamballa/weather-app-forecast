3d6cd4c4e04d6ec1fe074d2e2ab94a55a90d554c
wamballa <wamballa@Main-MBP.home>
2019-04-04 22:30:31 +0100

    sorted out creating city links

--- src/components/weather/CitySummary.js
+++ src/components/weather/CitySummary.js
@@ -2,25 +2,27 @@ import React, { Component } from 'react'
 
 export class CitySummary extends Component {
 
-  onlyUnique(value, index, self) {
-    return self.indexOf(value) === index;
+  handleFormSubmit(e) {
+    e.preventDefault();
+    console.log("Form submitted " + e.target.name);
+    // this.props.city = e.target.name;
+    console.log("form submitted props "+e.target.props);
   }
-  handleFormSubmit() {
-    console.log("Form submitted");
-  }
-
+  // <div key={city.id}> {city.name} {city.sys.country} Coords: {city.coord.lat}, {city.coord.lon}</div>
   render() {
     let cityList = this.props.cityList.list || []; // if no items give an empty array
-
-    console.log("CitySummary citylist...", cityList);
+    // console.log("CitySummary citylist...", cityList);
 
     return (
       <div>
-        <form  onSubmit={this.handleFormSubmit}>
+        <form onSubmit={this.handleFormSubmit}>
 
           {cityList.map((city) => (
             // how can i make these clickable?
-            <div key={city.id}> {city.name} {city.sys.country} Coords: {city.coord.lat}, {city.coord.lon}</div>
+            <div key={city.id}>
+              <input type="button" className="link-button" name={city.id} value={city.name} onClick={this.handleFormSubmit}></input>
+            </div>
+
           ))}
 
         </form>
