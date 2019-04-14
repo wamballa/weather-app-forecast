800b1464f0d5d71cf2ddfa6a3484af1e3b52872f
wamballa <wamballa@Main-MBP.home>
2019-04-14 10:44:38 +0100

    updates showcard and deleted comments

--- src/components/weather/ShowCards.js
+++ src/components/weather/ShowCards.js
@@ -7,21 +7,7 @@ export class ShowCards extends Component {
 
   render() {
 
-    // console.log('showcards prop ',this.props.cards);
-    // console.log('showcards prop.... ',this.props.cards[0]);
-    let cardList = this.props.cards;
-    // console.log('showcards prop ',cardList);
-    // console.log('cardList '+cardList.length);
-    // if (cardList.length == 0){
-    //   console.log ("Card list empty",this.props.cards)
-    // } else {
-    //   console.log('card list not empty.... ',this.props.cards);
-
-    // }
-    // console.log('showcards prop.... ',this.props.cards[0]);
-    // console.log('showcards icon ',cardList[0].icon);
-    // const icon_url = 'http://openweathermap.org/img/w/' + this.props.cards.icon + '.png';
-    // console.log('Showcard props '+icon_url);
+    const cardList = this.props.cards;
 
     return (
       <div>
